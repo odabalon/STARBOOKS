f9a82c87374c3178b2baff3468a567e5c9a05626
odabalon <abalonjonathan@gmail.com>
2018-06-14 15:39:52 +0800

    Added Home Page and Toolbar for all screen sizes

--- app/src/main/java/dost/itu/stii/starbooks_app/MainActivity.java
+++ app/src/main/java/dost/itu/stii/starbooks_app/MainActivity.java
@@ -2,39 +2,40 @@ package dost.itu.stii.starbooks_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.MediaController;
-import android.widget.ProgressBar;
-import android.widget.VideoView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.WindowManager;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private VideoView videoView;
-    private ProgressBar progressBar;
-    private MediaController mediaController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         init();
 
     }
 
     private void init() {
-//        videoView.setMediaController(mediaController);
-//        videoView.setVideoPath("http://192.168.0.102/starbooks/fullvideo/flv/Vid_Y003.flv");
-//
-//        videoView.setOnPreparedListener(new io.vov.vitamio.MediaPlayer.OnPreparedListener() {
-//            @Override
-//            public void onPrepared(io.vov.vitamio.MediaPlayer mp) {
-//                progressBar.setVisibility(View.GONE);
-//                mp.setPlaybackSpeed(1.0f);
-//            }
-//        });
-        videoView = findViewById(R.id.videoView);
-        progressBar = findViewById(R.id.progBar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation_menu,menu);
+        return true;
     }
 }
