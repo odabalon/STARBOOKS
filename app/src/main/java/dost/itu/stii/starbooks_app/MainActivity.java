b670136fb6206bbf28b0e93f5568da9a9899c433
odabalon <abalonjonathan@gmail.com>
2018-06-14 16:33:07 +0800

    Working Bottom Navigation

--- app/src/main/java/dost/itu/stii/starbooks_app/MainActivity.java
+++ app/src/main/java/dost/itu/stii/starbooks_app/MainActivity.java
@@ -1,17 +1,20 @@
 package dost.itu.stii.starbooks_app;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.WindowManager;
-
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-
-
+    BottomNavigationView bottomNavigationView;
+    TextView sampleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +23,39 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         init();
 
-    }
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId()==R.id.home){
+                    sampleText.setText("Home");
 
+                }else if(item.getItemId()==R.id.search){
+                    sampleText.setText("Search");
+
+                }else if(item.getItemId()==R.id.camera){
+                    sampleText.setText("Camera");
+
+                }else if(item.getItemId()==R.id.like){
+                    sampleText.setText("Like");
+
+                }else if(item.getItemId()==R.id.profile){
+                    sampleText.setText("Profile");
+
+                }
+                return true;
+            }
+        });
+
+    }
+    //Initializing Layout
     private void init() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        sampleText = findViewById(R.id.textDisplay);
 
 
 
